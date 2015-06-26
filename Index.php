9a8f68dee49ab5486b3e3be08fb8675c6de34919
klebcr24 <klebcr24@gmail.com>
2015-06-26 03:11:36 +0200

    Creacion de login

--- Index.php
+++ Index.php
@@ -1,10 +1,99 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
+  <head>
 
-</body>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Proyecto Multimedios</title>
+
+    <!-- Bootstrap -->
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body class="bg-info">
+
+
+  <!--Informacion Principal del Proyecto -->
+  <div style="font-family: cursive">
+   <div style="Text-Align:center">
+    <h1 style="color: #4e818b">Proyecto Multimedios 2015</h1>
+    <h1 style="color: #4e818b">Integrantes:</h1>
+    <h1 style="color: #4e818b">Celina Rosales (carne).</h1>
+    <h1 style="color: #4e818b">Caleb Delgado B12196.</h1>
+    <h1 style="color: #4e818b">Priscilla Salas (Carne).</h1>
+    <h1 style="color: #4e818b">Andrey Barahona (carne).</h1>
+    </div>
+    </div>
+    <p><br/></p>
+
+    <!--Construccion del Login-->
+
+	<div class="row">
+  		<div class="col-md-4"></div>
+  		<div class="col-md-5">
+  			<div class="panel panel-default">
+  				<div class="panel-body">
+    					<div class="page-header" style="margin-top:5px;">
+    						<h3>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Inicio de Sesion</h3>
+    					</div>
+    					<form class="form-horizontal" role="form">
+  						<div class="form-group">
+    				<label style="text-Align:left" for="inputfn3" class="col-sm-2 control-label">Usuario:</label>
+    							<div class="col-sm-10">
+    								<div class="input-group">
+    									<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+      								<input type="user" class="form-control" id="inputfn3" placeholder="">
+      							</div>
+    							</div>
+  						</div>
+  						<div class="form-group">
+    							<label for="inputEmail3" class="col-sm-2 control-label">Contraseña:</label>
+    							<div class="col-sm-10">
+    								<div class="input-group">
+    									<span class="input-group-addon">***</span>
+      								<input type="email" class="form-control" id="inputEmail3" placeholder="">
+      							</div>
+    							</div>
+  						</div>
+  						<div class="form-group">
+    							<div class="col-sm-10">
+    								<div class="input-group">
+
+
+      							</div>
+    							</div>
+  						</div>
+  						<div class="form-group">
+    							<div class="col-sm-offset-2 col-sm-10">
+      							<button type="submit" class="btn btn-primary">Ingresar</button>
+      							<button type="submit" class="btn btn-primary">Ingresar</button>
+    							</div>
+    							<div class="placeholder-100pct">----------------</div>
+  						</div>
+					</form>
+  				</div>
+			</div>
+  		</div>
+  		<div class="col-md-2"></div>
+	</div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <footer>
+       <div style="font: oblique bold 120% cursive">
+        <h1 style="text-Align:center"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Universidad de Costa Rica</h1>
+        <h1 style="text-Align:center">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Todos los derechos reservados © 2015</h1>
+        </div>
+
+    </footer>
+  </body>
 </html>
