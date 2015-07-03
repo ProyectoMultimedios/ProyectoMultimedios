7ad23f4d8d43c5737102d45927b3d605a1e73820
CelinaRosales <celinar94@outlook.es>
2015-07-02 18:43:06 -0600

    a

--- MantenimientoEstudiantes/InsertarEstudiante.php
+++ MantenimientoEstudiantes/InsertarEstudiante.php
@@ -27,22 +27,20 @@ if (!$conn) {
 $conn= mysql_connect ($servername, $username, $password);
 mysql_select_db('proyecto');
 
-$sql = "INSERT INTO proyecto.padre ( Nombre, Apellido, Identificacion, telefono )
-VALUES ('$NombreEncargado', '$ApellidoEncargado', '$Identificacion','$Telefono')";
+$sql = "INSERT INTO proyecto.padre ( Nombre, Apellido, Identificacion, telefono, Estado )
+VALUES ('$NombreEncargado', '$ApellidoEncargado', '$Identificacion','$Telefono','Activo')";
 
 $sql1 = "SELECT Id_Padre FROM padre";
 
 $Padre= mysql_query($sql) or die ('error'. mysql_error());
 $select= mysql_query($sql1) or die ('error'. mysql_error());
 $select = mysql_fetch_array($select);
-echo "$select[0]";
 
-$sql2 = "INSERT INTO proyecto.alumnos (Nombre, Apellido, Carnet,Genero,Id_Padre )
-VALUES ('$Nombre', '$Apellido', '$Carnet','Mujer', $select[0] )";
+
+
+$sql2 = "INSERT INTO proyecto.alumnos (Nombre, Apellido, Carnet,Genero,Id_Padre, Estado)
+VALUES ('$Nombre', '$Apellido', '$Carnet','Mujer', $select[0],'Activo')";
 
 $alumno= mysql_query($sql2) or die ('error'. mysql_error());
-
-
-
-
+header("Location: index.php");
 ?>
