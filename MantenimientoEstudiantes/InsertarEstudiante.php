22be1450d2ad8ee1a3755895a10a2f5597024c3c
CelinaRosales <celinar94@outlook.es>
2015-07-03 16:30:23 -0600

    ingresa

--- MantenimientoEstudiantes/InsertarEstudiante.php
+++ MantenimientoEstudiantes/InsertarEstudiante.php
@@ -31,15 +31,15 @@ mysql_select_db('multimedios2.0');
 $sql = "INSERT INTO padres (Nombre, Apellido1,Apellido2, Identificacion, telefono, Estado )
 VALUES('$NombreEncargado','$ApellidoEncargado','$Apellido2Encargado','$Identificacion','$Telefono','Activo')";
 
-$sql1 = "SELECT IdPadres FROM padres where identificacion = '$Identificacion''";
+$sql1 = "SELECT IdPadres FROM padres where Identificacion = '$Identificacion' ";
 
 $Padre= mysql_query($sql) or die ('error'. mysql_error());
 $select= mysql_query($sql1) or die ('error'. mysql_error());
 $select = mysql_fetch_array($select);
 
 
-$sql2 = "INSERT INTO estudiantes (Nombre, Apellido,Apellido2, Carnet,Genero,IdPadre, Estado)
-VALUES ('$Nombre', '$Apellido','$Apellido2', '$Carnet','Mujer', $select[0],'Activo')";
+$sql2 = "INSERT INTO estudiantes (IdPadres, Nombre, Apellido1,Apellido2, Carnet,Genero, Estado)
+VALUES ( $select[0],'$Nombre', '$Apellido','$Apellido2', '$Carnet','Mujer','Activo')";
 
 $alumno= mysql_query($sql2) or die ('error'. mysql_error());
 header("Location: index.php");
