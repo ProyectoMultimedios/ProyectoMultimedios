490105a36533228dc62f2072ae4c2bf787cf591e
PriscillaSalas <priscillasalas91@gmail.com>
2015-07-05 01:07:23 -0600

    dfd

--- MantenimientoAdministrador/buscarProfesor.php
+++ MantenimientoAdministrador/buscarProfesor.php
@@ -1,26 +1,48 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8" />
-	<title>Buscar Profesor - Formoid css form</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <title>Buscar Profesor</title>
+    <link rel="stylesheet" href="estilos.css" >
+
 </head>
-<body class="blurBg-false" style="background-color:#EBEBEB">
+
+<body>
+
+     <img id="bg" src="BuscarProfesor.png"  alt="background">
+
+     <form class="form-horizontal" role="form" method="post" action="ModificarProfesor.php" >
 
 
+            <div>
+             <h1>Ingrese los Datos Del Profesor a Buscar</h1>
 
-<!-- Start Formoid form-->
-<link rel="stylesheet" href="buscar_files/formoid1/formoid-solid-dark.css" type="text/css" />
-<script type="text/javascript" src="buscar_files/formoid1/jquery.min.js"></script>
-<form class="formoid-solid-dark" style="background-color:#FFFFFF;font-size:13px;font-family:'Open Sans',Arial,Verdana,sans-serif;color:#34495E;max-width:480px;min-width:150px" method="post" action="ModificarProfesor.php"><div class="title"><h2>Buscar Profesor</h2></div>
-	<div class="element-input"><label class="title"></label><div class="item-cont"><input class="large" type="text" name="input" placeholder="Ingrese la cedula a buscar"/><span class="icon-place"></span></div></div>
-<h4><li><a href="IndexAdministrador.php">
-Atras
-</a></li></h4>
-<div class="submit"><input type="submit" value="Buscar"/></div></form><p class="frmd"><a href="http://formoid.com/v29.php">css form</a> Formoid.com 2.9</p><script type="text/javascript" src="buscar_files/formoid1/formoid-solid-dark.js"></script>
-<!-- Stop Formoid form-->
+             <br><label for="" alt="lblCedula"><b>Numero de cedula:</b> </label>
+      <input type="text" alt="TxtCedula" name="Cedula"> <br> <br>
 
+<input type="submit" value="Aceptar" >
+           <input type="submit" value="Limpiar" >
+
+         <br> <br>
 
 
+       </div>
+
+       <div1>
+       <nav>
+
+          <ul class="nav nav-pills pull-right">
+
+            <li role="presentation" ><a href="IndexAdministrador.php"><h1>Volver</h1></a></li>
+          </ul>
+        </nav>
+         </div1>
+
+      <br><br><br>
+
+</form>
 </body>
+
 </html>
+
+
